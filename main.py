65656640b3568f6543f1584110dcd15e75da36df
Michal Moudrý <moudry.michal@live.com>
2023-01-16 14:10:33 +0100

    Changes to main

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ import easyocr
 
 if __name__ == "__main__":
     reader = easyocr.Reader(["en"])
-    result = reader.readtext("./test_images/img01.png")
+    result = (
+        reader.readtext("./test_images/img01.png"),
+        reader.readtext("./test_images/img02.png")
+    )
     print("Result:", result)
