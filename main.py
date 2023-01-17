1b492de5067fbda01fe687d01959188133c1b9d7
Michal Moudrý <moudry.michal@live.com>
2023-01-17 09:05:16 +0100

    Added cs lang

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Python OCR app sample.
 import easyocr
 
 if __name__ == "__main__":
-    reader = easyocr.Reader(["en"])
+    reader = easyocr.Reader(["en", "cs"])
     result = (
         reader.readtext("./test_images/img01.png"),
         reader.readtext("./test_images/img02.png")
